138fdd19ac1878ebcfbd45e469cacd2bbfe702f3
Allison Lyon <alyoness1@gmail.com>
2016-08-17 16:21:28 -0700

    2.1

---
+++ chapter2.py
@@ -0,0 +1,19 @@
+# 2.1
+# Runtime: 
+# Space:
+# Edge cases: 
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n ALL TESTS PASSED!! \n"
