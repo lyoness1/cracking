5c82b3044642e4cf479180578e8aa41546edba7c
lyoness1 <alyoness1@gmail.com>
2016-09-13 14:17:20 -0700

    3.1

--- chapter3.py
+++ chapter3.py
@@ -40,3 +40,18 @@ class SingleArrayStacks:
 
 
 
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        return self.stack.pop()
+
+    def peek(self):
+        return self.stack[-1]
+
+    def isEmpty(self):
+        return self.stack == []
