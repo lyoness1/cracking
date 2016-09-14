4e6830e21c32e2cd60069f6d0f703eb8a2c4290d
lyoness1 <alyoness1@gmail.com>
2016-09-14 15:27:33 -0700

    4.1

--- chapter4.py
+++ chapter4.py
@@ -1,54 +1,62 @@
 # TREES AND GRAPHS
 
+# 4.1 Write a method to check for balance - no leafs differ by more than depth 1
+"""
+    >>> unbalanced = Node('0', [Node('0_0', [Node('0_0_0', [Node("extra")]), Node('0_0_1')]), Node('0_1', [Node('0_1_0')]), Node('0_2')])
+    >>> unbalanced.check_for_balance()
+    False
+    >>> balanced = Node('0', [Node('0_0', [Node('0_0_0'), Node('0_0_1')]), Node('0_1', [Node('0_1_0')]), Node('0_2')])
+    >>> balanced.check_for_balance()
+    True
+    >>> Node().check_for_balance()
+    True
+
+"""
 
 # Generic Tree Structure
-class Node:
-    def __init__(self, value):
+class Node(object):
+    def __init__(self, value=None, children=None):
         self.value = value
-        self.children = []
+        self.children = children or []
 
-    def add_child(self, value):
-        node = Node(value)
-        self.children.append(node)
+    def __str__(self, level=0):
+        ret = '\t' * level + repr(self.value)
+        for child in self.children:
+            ret += '\n' + child.__str__(level+1)
+        return ret
 
-
-# Write a fn to determine if a tree is balanced
-class BalancedTree:
-    def __init__(self, root):
-        self.root = root
+    def __repr__(self):
+        return self.value
 
     def check_for_balance(self):
-        if not self.root:
-            return True
+        return (self.get_max_depth() - self.get_min_depth() < 2)
 
-        # depth first search using to_visit stack
-        to_visit = [self.root]
-        # dictionary to track depth counts for each leaf
-        depths = {}
-        # initialize depth=0 for root
-        depth = 0
+    def get_min_depth(self):
+        if not self.children:
+            return 0
+        return 1 + min(child.get_min_depth() for child in self.children)
 
-        while to_visit:
-            node = to_visit.pop()
-            # base case: found leaf, update depths dict
-            if not node.children:
-                if depth in depths:
-                    depths[depth] += 1
-                else: 
-                    depths[depth] = 1
-            # progress: add children to to_visit stack
-            else: 
-                to_visit.extend(node.children)
+    def get_max_depth(self):
+        if not self.children: 
+            return 0
+        return 1 + max(child.get_max_depth() for child in self.children)  
 
-        # analyze tree depths
-        keys = depths.keys()
-        if len(keys) < 1 or abs(keys[1] - keys[0]) < 2:
-            return True
-        else: 
-            return False
+
+# 4.2 Given a directed graph, design an algorithm to find out whether there is a
+# route be- tween two nodes. 
+"""
+    >>> 
+
+"""
+class DirectedGraphNode(object):
+    def __init__(self)
 
 
 
 
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n\n ALL TESTS PASSED!! \n\n"
 
         
